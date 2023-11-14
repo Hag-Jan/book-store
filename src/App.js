7ac61450051d917851935803081160e119275b69
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-14 18:36:25 +0100

    Modify App.js and HeaderTop.jsx, add Slider component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 import Header from './components/header/Header';
+import Slider from './components/slider/Slider';
 
 function App() {
   return (
-    <Header />
+    <div>
+
+      <Header />
+      <Slider />
+    </div>
   );
 }
 
--- src/components/header/HeaderTop.jsx
+++ src/components/header/HeaderTop.jsx
@@ -1,8 +1,10 @@
+
+
 const HeaderTop = ({ setToggle, toggle }) => {
     return (
         <div className="header-top">
             <div onClick={() => setToggle(prev => !prev)} className="header-top-menu">
-               {toggle ?  <i className="bi bi-x-lg"></i> :  <i className="bi bi-list"></i>}
+                {toggle ? <i className="bi bi-x-lg"></i> : <i className="bi bi-list"></i>}
             </div>
             <div className="header-top-phone">
                 <i className="bi bi-telephone-fill"></i>
