2963bba0d67031ac08ff4fa71ae22cd45dd0502f
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-14 15:04:19 +0100

    Updated and deleted files, added new directories and files

---
+++ src/components/header/HeaderMiddle.jsx
@@ -0,0 +1,20 @@
+const HeaderMiddle = () => {
+    return (
+        <div className="header-middle">
+            <div className="header-middle-logo">
+                <b>Book</b>
+                <i className="bi bi-book"></i>
+                <b>Store</b>
+            </div>
+            <div className="header-middle-search-box">
+                <input className="header-middle-search-input" typeof="search" placeholder="Search in book store" />
+                <i className="bi bi-search"></i>
+            </div>
+            <div className="header-middle-cart-wrapper">
+                <i className="bi bi-cart2"></i>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderMiddle;
---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = ({ toggle, setToggle }) => {
+    return (
+        <nav style={{ left: toggle && "0" }} className="navbar">
+            <ul className="navbar-links">
+                <li onClick={() => setToggle(false)} className="navbar-link">Home</li>
+                <li onClick={() => setToggle(false)} className="navbar-link">Authors</li>
+                <li onClick={() => setToggle(false)} className="navbar-link">About Us</li>
+                <li onClick={() => setToggle(false)} className="navbar-link">Register</li>
+                <li onClick={() => setToggle(false)} className="navbar-link">Contact Us</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
