baf6f03e3744a7ee880c7342f35f4822ce7d81b7
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-15 16:37:37 +0100

    Commit message describing changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-
 import './App.css';
+import BookSlider from './components/book-slider/BookSlider';
 import Header from './components/header/Header';
+import HeadingTitle from './components/heading-title/HeadingTitle';
 import Services from './components/services/Services';
 import Slider from './components/slider/Slider';
+import { books } from "./data/books"
 
 function App() {
   return (
     <div>
-
       <Header />
       <Slider />
-      <Services/>
+      <Services />
+      <HeadingTitle title="Most Gifted" />
+      <BookSlider data={books} />
+      <HeadingTitle title="Best seller" />
+      <BookSlider data={books} />
+      <HeadingTitle title="Most wished for" />
+      <BookSlider data={books} />
     </div>
   );
 }
