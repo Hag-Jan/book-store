995340df7caa24cf69d60a8c5219edd30db3a831
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-27 19:04:10 +0100

    Add CartItem OrderSummary car.css Files

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,26 @@
+import { cartInfo } from "../../data/cart"
+import CartItem from "./CartItem";
+import OrderSummary from "./OrderSummary";
+import "./cart.css"
+
+
 const Cart = () => {
-    return ( 
-        <h1>CArt</h1>
-     );
+
+    const totalPrice = cartInfo.reduce((acc, cur) => acc + cur.price * cur.quantity, 0)
+
+    return (
+        <div className="cart">
+            <h1 className="cart-title">Your Shopping Cart</h1>
+            <div className="cart-wrapper">
+                <div className="cart-items">
+                    {cartInfo.map((item) =>
+                        <CartItem key={item.id} item={item} />
+                    )}
+                </div>
+                <OrderSummary totalPrice={totalPrice} />
+            </div>
+        </div>
+    );
 }
- 
+
 export default Cart;
